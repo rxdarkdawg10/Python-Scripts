2c3a28c966116c73886a5abaad405fc48101d2fd
rxdarkdawg10 <choncey@gmail.com>
2013-10-15 10:17:17 -0600

    Update Tableau TDE API with Teradata Connection.py
    Turned into proper program (using functions) and added help and parameter functionality. 
    Added process for adding extract file to existing data source as well.

--- Tableau TDE API with Teradata Connection.py
+++ Tableau TDE API with Teradata Connection.py
@@ -1,54 +1,64 @@
 import dataextract as tde
-import pyodbc as td
-import getpass, os, datetime, csv, time
-from collections import OrderedDict
+import pyodbc as odbc
+import getpass, os, datetime, csv, time, sys, getopt, subprocess
 
-# Get Teradata User Information
-uid = raw_input("Enter UID for Teradata: ")
-pwd = getpass.getpass()
+global dir
+global tp
+global ts
+global td
+global outputfile
+global fiscalWeek
+global startTime
 
-# Start Timer
-startTime = time.clock()
+def runQuery():
+    startTime = time.clock()
 
-# Set Teradata Connection
-print "Connecting to Teradata..."
-conn = td.connect('DRIVER={TERADATA};DBCNAME=<DBCNAME>;UID='+uid+';PWD='+pwd+';QUIETMODE=YES;')
+    # Get Teradata User Information
+    uid = raw_input("Enter UID for Teradata: ")
+    pwd = getpass.getpass()
 
-# Get SQL Query --if left empty, will run imbedded query
-query = raw_input("Enter query to run: ")
+    # Set Teradata Connection
+    print "Connecting to Teradata..."
+    conn = odbc.connect('DRIVER={TERADATA};DBCNAME=<DBCNAME;UID='+uid+';PWD='+pwd+';QUIETMODE=YES;')
 
-# Create cursor for SELECT Query
-curs = conn.cursor()
+    # Create cursor for SELECT Query
+    curs = conn.cursor()
 
-# Display Progress
-print "Executing Query..."
+    # Display Progress
+    print "Executing Query..."
 
-# Set Query
-if query == "":
+    # Set Query
     curs.execute("""SELECT * FROM FOOBAR""")
-else:
-    curs.execute(query)
 
-# Create new CSV File for Consumption
 
-csvName = raw_input("Give the file a name :")
-try:
-    csvFile = open(csvName+'.csv','wb')
-except:
-    os.remove(csvName+'.csv')
-    csvFile = open(csvName+'.csv','wb')
+    ## File Loop for CSV Adjustment / Moves
+    #fileDir = dir
+    #for root, dirs, filenames in os.walk(fileDir):
+    #    for file in filenames:
+    #        if ".csv" in file:
+    #            os.rename(file, file.replace(".csv","<week/date>.csv",1))
+            
 
-# Push records into CSV file
-print "Writting to CSV file for Consumption"
+    # Create new CSV File for Consumption
+    csvName = outputfile
+    try:
+        csvFile = open(csvName+'.csv','wb')
+    except:
+        os.remove(csvName+'.csv')
+        csvFile = open(csvName+'.csv','wb')
 
-writer = csv.writer(csvName+'.csv')
-writer.writerow([i[0] for i in curs.description])
-writer.writerows(curs)
+    # Push records into CSV file
+    print "Writting to CSV file for Consumption"
+    print "%i records returned" % (curs.rowcount)
 
-# Close Connection 
-conn.close()
+    writer = csv.writer(csvFile)
+    writer.writerow([i[0] for i in curs.description])
+    writer.writerows(curs)
 
-csvFile.close()
+    # Close Connection 
+    conn.close()
+
+    csvFile.close()
 
 # TDE File Creation
 # TDE Functions
@@ -110,66 +120,158 @@ def add_tde_col(colnum, row, val, t):
 # Tableau DataTypes Dictionary Creation
 tdeTypes = {'INTEGER': 7, 'DOUBLE': 10, 'BOOLEAN': 11, 'DATE': 12, 'DATETIME': 13, 'DURATION': 14, 'CHAR_STRING': 15, 'UNICODE_STRING': 16}
 
-# This part is unique for each extract. 
-# Will need to modify.. 
-# TODO: Make this more dynamic
-################################################################################################
-################################################################################################
+def createTDEFile():
+    # This part is unique for each extract. 
+    # Will need to modify.. 
+    # TODO: Make this more dynamic
+    ################################################################################################
+    ################################################################################################
 
-csvSchema = []
-csvSchema.append({'<COLUMNNAME>': tdeTypes['INTEGER']})
+    csvSchema = []
+    csvSchema.append({'<COLUMNNAME>': tdeTypes['INTEGER']})
 
-################################################################################################
-################################################################################################
+    ################################################################################################
+    ################################################################################################
 
-# Try to create the TDE File
-try:
-    tdeFile = tde.Extract(csvName+'.tde')
+    # Try to create the TDE File
+    try:
+        tdeFile = tde.Extract(outputfile+'.tde')
 
-except:
-    os.remove(csvName+'.tde')
-    tdeFile = tde.Extract(csvName+'.tde')
+    except:
+        os.remove(outputfile+'.tde')
+        tdeFile = tde.Extract(outputfile+'.tde')
 
 
-# Open newly created CSV File
-csvFile = open(csvName+'.csv',"r")
-reader = csv.reader(csvFile)
-print 'Reading records from %s' % (csvName+'.csv')
+    # Open newly created CSV File
+    csvFile = open(outputfile+'.csv',"r")
+    reader = csv.reader(csvFile)
+    print 'Reading records from %s' % (outputfile+'.csv')
 
-# Create TDE Table definition
-tdeTableDef = tde.TableDefinition()
+    # Create TDE Table definition
+    tdeTableDef = tde.TableDefinition()
 
-# Build TDE Table Definition from csv schema above
-print 'Defined table schema:'
-for index, item in enumerate(csvSchema):
-    for k, v in item.items():
-        print 'Column %i: %s <%s>' % (index, k, tdeTypes.keys()[tdeTypes.values().index(v)])
-        tdeTableDef.addColumn(k, v)
+    # Build TDE Table Definition from csv schema above
+    print 'Defined table schema:'
+    for index, item in enumerate(csvSchema):
+        for k, v in item.items():
+            print 'Column %i: %s <%s>' % (index, k, tdeTypes.keys()[tdeTypes.values().index(v)])
+            tdeTableDef.addColumn(k, v)
 
-# Add table to TDE File
-tdeTable = tdeFile.addTable("Extract", tdeTableDef)
+    # Add table to TDE File
+    tdeTable = tdeFile.addTable("Extract", tdeTableDef)
 
-# Iterate through rows and columns of csv and adding them to TDE File
-print 'Writing records to %s' % (csvName+'.tde')
+    # Iterate through rows and columns of csv and adding them to TDE File
+    print 'Writing records to %s' % (csvName+'.tde')
 
-rownum = 0
-for row in reader:
-    if rownum == 0:
-        header = row
+    rownum = 0
+    for row in reader:
+        if rownum == 0:
+            header = row
+        else:
+            colnum = 0
+            tdeRow = tde.Row(tdeTableDef)
+            for col in row:
+                if colnum+1 > len(csvSchema):
+                    print 'Something is missing here.'
+                    break
+                add_tde_col(colnum, tdeRow, row[colnum], csvSchema[colnum].values()[0])
+                colnum += 1
+            tdeTable.insert(tdeRow)
+            tdeRow.close()
+        rownum += 1
+
+    print '%i rows added in total in %f seconds' % (rownum-1, time.clock()-startTime)
+    print 'Closing TDE and CSV File...'
+    tdeFile.close()
+    csvFile.close()
+
+def RunUpdateProcess():
+    subprocess.check_output("C:\Program Files (x86)\Tableau\Tableau 8.0\bin\tableau addfiletoextract --file '"+ dir + "\\" + outputfile + '.csv' + "' --server "+ ts +" --project '"+ tp +"' --datasource '"+ td +"' --username <user> --password <pass>",
+                            stderror = subprocess.STDOUT,
+                            shell=False)
+    print stderror
+
+def main(argv):
+    dir = ''
+    tp = ''
+    ts = ''
+    td = ''
+    outputfile = ''
+    fiscalWeek = ''
+    try:
+        opts, args = getopt.getopt(argv, "hv:d:tp:ts:td:v:fw:o:",["help","directory=","tableau-project=","tableau-server=","tableau-datasource=","version=","fiscalweek","output-file="])
+    except getopt.GetoptError:
+        print 'Tableau Data Extract File Generation from Teradata using SQL Query'
+        print ''
+        print 'USAGE: file -d <directory> -tp <projectname>' 
+        print '       -ts <server-url> -td <datasourcename>'
+        print '       -o <datastorefilename>'
+        print '-h,  --help                  Returns this help file'
+        print '-v,  --version               Version information'
+        print '-d,  --directory             Directory of file store'
+        print '-tp, --tableau-project       Tableau Project Name'
+        print '-ts, --tableau-server        Tableau Server Address (http)'
+        print '-td, --tableau-datasource    Tableau Data Source Name'
+        print '-o,  --output-file           Filename to store data as.'
+        print '                             This file will be the naem of'
+        print '                             the file stored on the local'
+        print '                             system.'
+        print '-fw, --fiscalweek            Week for query to be ran for'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in("-h","--help"):
+            print 'Tableau Data Extract File Generation from Teradata using SQL Query'
+            print ''
+            print 'USAGE: file -d <directory> -tp <projectname>' 
+            print '       -ts <server-url> -td <datasourcename>'
+            print '       -o <datastorefilename>'
+            print '-h,  --help                  Returns this help file'
+            print '-v,  --version               Version information'
+            print '-d,  --directory             Directory of file store'
+            print '-tp, --tableau-project       Tableau Project Name'
+            print '-ts, --tableau-server        Tableau Server Address (http)'
+            print '-td, --tableau-datasource    Tableau Data Source Name'
+            print '-o,  --output-file           Filename to store data as.'
+            print '                             This file will be the naem of'
+            print '                             the file stored on the local'
+            print '                             system.'
+            print '-fw, --fiscalweek            Week for query to be ran for'
+            sys.exit()
+        elif opt in ("-d","--directory"):
+            dir = arg
+        elif opt in ("tp","--tableau-project"):
+            tp = arg
+        elif opt in ("ts","--tableau-server"):
+            ts = arg
+        elif opt in ("td","--tableau-datasource"):
+            td = arg
+        elif opt in ("-o","--output-file"):
+            outputfile = arg
+        elif opt in ("-fw","--fiscalweek"):
+            fiscalWeek = arg
+    if dir == '' or tp == '' or ts == '' or td == '' or outputfile == '' or fiscalWeek == '':
+        print 'Tableau Data Extract File Generation from Teradata using SQL Query'
+        print ''
+        print 'USAGE: file -d <directory> -tp <projectname>' 
+        print '       -ts <server-url> -td <datasourcename>'
+        print '       -o <datastorefilename>'
+        print '-h,  --help                  Returns this help file'
+        print '-v,  --version               Version information'
+        print '-d,  --directory             Directory of file store'
+        print '-tp, --tableau-project       Tableau Project Name'
+        print '-ts, --tableau-server        Tableau Server Address (http)'
+        print '-td, --tableau-datasource    Tableau Data Source Name'
+        print '-o,  --output-file           Filename to store data as.'
+        print '                             This file will be the naem of'
+        print '                             the file stored on the local'
+        print '                             system.'
+        print '-fw, --fiscalweek            Week for query to be ran for'
+        sys.exit()
     else:
-        colnum = 0
-        tdeRow = tde.Row(tdeTableDef)
-        for col in row:
-            if colnum+1 > len(csvSchema):
-                print 'Something is missing here.'
-                break
-            add_tde_col(colnum, tdeRow, row[colnum], csvSchema[colnum].values()[0])
-            colnum += 1
-        tdeTable.insert(tdeRow)
-        tdeRow.close()
-    rownum += 1
+        runQuery()
+        createTDEFile()
+        RunUpdateProcess()
 
-print '%i rows added in total in %f seconds' % (rownum-1, time.clock()-startTime)
-print 'Closing TDE and CSV File...'
-tdeFile.close()
-csvFile.close()
+# Main Load function
+if __name__ == "__main__":
+    main(sys.argv[1:])
